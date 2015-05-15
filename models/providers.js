9d3bb8cefc9f04c3b3e01404dd630bdfec0e3a72
Darren Miller <dazmiller@gmail.com>
2015-05-15 16:26:50 +1000

    inital

---
+++ models/providers.js
@@ -0,0 +1,32 @@
+providers = new Mongo.Collection('providers');
+
+providers.attachSchema(
+    new SimpleSchema({
+    title: {
+      type: String
+    },
+    content: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      denyUpdate: true
+    }
+  })
+);
+
+// Collection2 already does schema checking
+// Add custom permission rules if needed
+if (Meteor.isServer) {
+  providers.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
